7358b12d7bffb4fb04898b538b959579488e70e6
undiano <cur551997@gmail.com>
2019-01-29 18:45:24 +0100

    -Ventana de proyectos

--- src/main/java/view/CreateProject.java
+++ src/main/java/view/CreateProject.java
@@ -15,6 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
@@ -26,8 +27,10 @@ public class CreateProject extends JInternalFrame implements ActionListener {
 	private JPanel pCenterPanel;
 	private JDesktopPane pjdPanel;
 	private Usuari pUsuari;
+
 	private JLabel plNombreProyecto, plDescripcion, plScrumMaster, plProductOwner;
-	private JTextField ptfNombreProyecto, ptfDescripcion;
+	private JTextField ptfNombreProyecto;
+	private JTextArea ptfDescripcion;
 	private JComboBox<String> pcbScrumMaster, pcbProductOwner;
 	private JButton pbAnadir;
 
@@ -40,62 +43,109 @@ public class CreateProject extends JInternalFrame implements ActionListener {
 
 	public void view() {
 		// if(e.getSource() == Tittle.smiNewUser) {
-		pCenterPanel = new JPanel();
-		pCenterPanel.setLayout(new GridBagLayout());
-		GridBagConstraints constraints = new GridBagConstraints();
+				pCenterPanel = new JPanel();
+				pCenterPanel.setLayout(new GridBagLayout());
+				GridBagLayout gridBagLayout = new GridBagLayout();
+				gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+				gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 19, 0, 0, 0, 0, 0, 0, 0};
+				gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+				gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+				getContentPane().setLayout(gridBagLayout);
+				
+				plNombreProyecto = new JLabel("Nombre Proyecto: ");
+				GridBagConstraints gbc_plNombre = new GridBagConstraints();
+				gbc_plNombre.fill = GridBagConstraints.HORIZONTAL;
+				gbc_plNombre.insets = new Insets(0, 0, 5, 5);
+				gbc_plNombre.gridx = 1;
+				gbc_plNombre.gridy = 1;
+				getContentPane().add(plNombreProyecto, gbc_plNombre);
+				
+				ptfNombreProyecto = new JTextField();
+				GridBagConstraints gbc_ptfNombre = new GridBagConstraints();
+				gbc_ptfNombre.gridwidth = 2;
+				gbc_ptfNombre.insets = new Insets(0, 0, 5, 5);
+				gbc_ptfNombre.fill = GridBagConstraints.HORIZONTAL;
+				gbc_ptfNombre.gridx = 2;
+				gbc_ptfNombre.gridy = 1;
+				getContentPane().add(ptfNombreProyecto, gbc_ptfNombre);
+				ptfNombreProyecto.setColumns(10);
+				
+				plDescripcion = new JLabel("Descripci\u00F3n:");
+				GridBagConstraints gbc_plDescripcin = new GridBagConstraints();
+				gbc_plDescripcin.fill = GridBagConstraints.HORIZONTAL;
+				gbc_plDescripcin.insets = new Insets(0, 0, 5, 5);
+				gbc_plDescripcin.anchor = GridBagConstraints.EAST;
+				gbc_plDescripcin.gridx = 1;
+				gbc_plDescripcin.gridy = 3;
+				getContentPane().add(plDescripcion, gbc_plDescripcin);
+				
+				ptfDescripcion = new JTextArea();
+				GridBagConstraints gbc_textArea = new GridBagConstraints();
+				gbc_textArea.gridheight = 2;
+				gbc_textArea.gridwidth = 2;
+				gbc_textArea.insets = new Insets(0, 0, 5, 5);
+				gbc_textArea.fill = GridBagConstraints.BOTH;
+				gbc_textArea.gridx = 2;
+				gbc_textArea.gridy = 3;
+				getContentPane().add(ptfDescripcion, gbc_textArea);
+				
+				plScrumMaster = new JLabel("Scrum Master:");
+				GridBagConstraints gbc_plScrumMaster = new GridBagConstraints();
+				gbc_plScrumMaster.fill = GridBagConstraints.HORIZONTAL;
+				gbc_plScrumMaster.anchor = GridBagConstraints.EAST;
+				gbc_plScrumMaster.insets = new Insets(0, 0, 5, 5);
+				gbc_plScrumMaster.gridx = 1;
+				gbc_plScrumMaster.gridy = 6;
+				getContentPane().add(plScrumMaster, gbc_plScrumMaster);
+				
+				pcbScrumMaster = new JComboBox();
+				GridBagConstraints gbc_pcbProductOwner = new GridBagConstraints();
+				gbc_pcbProductOwner.gridwidth = 2;
+				gbc_pcbProductOwner.insets = new Insets(0, 0, 5, 5);
+				gbc_pcbProductOwner.fill = GridBagConstraints.HORIZONTAL;
+				gbc_pcbProductOwner.gridx = 2;
+				gbc_pcbProductOwner.gridy = 6;
+				getContentPane().add(pcbScrumMaster, gbc_pcbProductOwner);
+				
+				plProductOwner = new JLabel("Product Owner: ");
+				GridBagConstraints gbc_plProductOwner = new GridBagConstraints();
+				gbc_plProductOwner.fill = GridBagConstraints.HORIZONTAL;
+				gbc_plProductOwner.anchor = GridBagConstraints.EAST;
+				gbc_plProductOwner.insets = new Insets(0, 0, 5, 5);
+				gbc_plProductOwner.gridx = 1;
+				gbc_plProductOwner.gridy = 8;
+				getContentPane().add(plProductOwner, gbc_plProductOwner);
+				
+				pcbProductOwner = new JComboBox();
+				GridBagConstraints gbc_comboBox = new GridBagConstraints();
+				gbc_comboBox.gridwidth = 2;
+				gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+				gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+				gbc_comboBox.gridx = 2;
+				gbc_comboBox.gridy = 8;
+				getContentPane().add(pcbProductOwner, gbc_comboBox);
+				
+				pbAnadir = new JButton("A\u00F1adir:");
+				GridBagConstraints gbc_pbAnadir = new GridBagConstraints();
+				gbc_pbAnadir.anchor = GridBagConstraints.WEST;
+				gbc_pbAnadir.insets = new Insets(0, 0, 5, 5);
+				gbc_pbAnadir.gridx = 2;
+				gbc_pbAnadir.gridy = 10;
+				getContentPane().add(pbAnadir, gbc_pbAnadir);
+				
+				// dimension longitud cuadro textos
+						ptfNombreProyecto.setColumns(15);
+						//constraints.gridwidth = 1;
 
-		constraints.insets = new Insets(10, 10, 10, 10);
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-
-		plNombreProyecto = new JLabel("Nombre");// hard
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		pCenterPanel.add(plNombreProyecto, constraints);
-		ptfNombreProyecto = new JTextField();
-		constraints.gridx = 1;
-		constraints.gridy = 0;
-		pCenterPanel.add(ptfNombreProyecto, constraints);
-
-		plDescripcion = new JLabel("Descripcion");
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		pCenterPanel.add(plDescripcion, constraints);
-		ptfDescripcion = new JTextField();
-		constraints.gridx = 1;
-		constraints.gridy = 1;
-		pCenterPanel.add(ptfDescripcion, constraints);
-
-		pbAnadir = new JButton("Anadir");// hard
-		constraints.gridx = 1;
-		constraints.gridy = 5;
-		constraints.gridwidth = 2;
-		pCenterPanel.add(pbAnadir, constraints);
-		pbAnadir.addActionListener(this);
-
-		// dimension longitud cuadro textos
-		ptfNombreProyecto.setColumns(15);
-		constraints.gridwidth = 1;
-
-		pcbProductOwner = new JComboBox<String>();
-		constraints.gridx = 0;
-		constraints.gridy = 5;
-		pCenterPanel.add(pcbProductOwner, constraints);
-//		project.getUsers("ProductOwner");//Metodo para coger a los ProductOwners
-	
-		pcbScrumMaster = new JComboBox<String>();
-		constraints.gridx = 0;
-		constraints.gridy = 5;
-		pCenterPanel.add(pcbScrumMaster, constraints);
-//		project.getUsers("ScrumMaster");//Metodo para coger a los ScrumMasters
-		
-		setTitle("Nuevo Proyecto");// hard
-		setResizable(true);
-		setClosable(true);
-//		setSize(this.pFrame.getWidth()/2,this.pFrame.getHeight()/2);
-//		setLocation(pFrame.getHeight()/2-this.getHeight()/2, pFrame.getWidth()/2-this.getWidth()/2);
-		setVisible(true);
-		setSize(680,532);
-		add(pCenterPanel, BorderLayout.CENTER);
+				
+				setTitle("Nuevo Proyecto");// hard
+				setResizable(true);
+				setClosable(true);
+//				setSize(this.pFrame.getWidth()/2,this.pFrame.getHeight()/2);
+//				setLocation(pFrame.getHeight()/2-this.getHeight()/2, pFrame.getWidth()/2-this.getWidth()/2);
+				setVisible(true);
+				setSize(680,532);
+				add(pCenterPanel, BorderLayout.CENTER);
 	}
 
 	public void actionPerformed(ActionEvent e) {
